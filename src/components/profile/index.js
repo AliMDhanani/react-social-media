8e84150d06b02c4dec89b4757cdb3aeefe4e46a8
AliMDhanani <alidhanani2000@gmail.com>
2023-02-01 20:30:18 +0000

    profile page and avatar upload funtionality

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -1,6 +1,49 @@
+import { Button, Divider, Flex, HStack, Stack, Text, useDisclosure } from "@chakra-ui/react";
+import PostsList from "components/post/PostsList";
+import { usePosts } from "hooks/posts";
+import { useUser } from "hooks/users";
+import { useParams } from "react-router-dom";
+import Avatar from "./Avatar";
+import { format } from "date-fns";
+import EditProfile from "./EditProfile";
 
-export default function index() {
+export default function Profile() {
+  const { id } = useParams();
+  const { posts, isLoading: postsLoading } = usePosts(id);
+  const { user, isLoading: userLoading } = useUser(id);
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
+  if (userLoading) return "loading user data...";
+
   return (
-    <div>index</div>
-  )
+    <Stack spacing="5">
+      <Flex p={["4", "6"]} pos="relative" align="center">
+        <Avatar size="2xl" user={user} />
+
+        <Button pos="absolute" mb="2" top="6" right="6" colorScheme="green" onClick={onOpen}>
+          Update Image
+        </Button>
+        <Stack ml="10">
+          <Text fontSize="2xl">{user.username}</Text>
+          <HStack spacing="10">
+            <Text color="gray.700" fontSize={["sm", "lg"]}>
+              Posts: {posts.length}
+            </Text>
+            <Text color="gray.700" fontSize={["sm", "lg"]}></Text>
+            <Text color="gray.700" fontSize={["sm", "lg"]}>
+              Joined: {format(user.date, "MMM YYY")}
+            </Text>
+          </HStack>
+        </Stack>
+        <EditProfile isOpen={isOpen} onClose={onClose} />
+      </Flex>
+      <Divider />
+
+      {postsLoading ? (
+        <Text>Posts are loading...</Text>
+      ) : (
+        <PostsList posts={posts} />
+      )}
+    </Stack>
+  );
 }
--- src/hooks/posts.js
+++ src/hooks/posts.js
@@ -9,10 +9,14 @@ import {
   query,
   setDoc,
   updateDoc,
+  where,
 } from "firebase/firestore";
 import { db } from "lib/firebase";
 import { useState } from "react";
-import { useCollectionData, useDocumentData } from "react-firebase-hooks/firestore";
+import {
+  useCollectionData,
+  useDocumentData,
+} from "react-firebase-hooks/firestore";
 
 export function useAddPost() {
   const [isLoading, setLoading] = useState(false);
@@ -40,8 +44,14 @@ export function useAddPost() {
   return { addPost, isLoading };
 }
 
-export function usePosts() {
-  const q = query(collection(db, "posts"), orderBy("date", "desc"));
+export function usePosts(uid = null) {
+  const q = uid
+    ? query(
+        collection(db, "posts"),
+        orderBy("date", "desc"),
+        where("uid", "==", uid)
+      )
+    : query(collection(db, "posts"), orderBy("date", "desc"));
   const [posts, isLoading, error] = useCollectionData(q);
   if (error) throw error;
   return { posts, isLoading };
@@ -71,8 +81,8 @@ export function useDeletePost(id) {
 }
 
 export function usePost(id) {
-  const q = doc(db, "posts", id)
-  const [post, isLoading ] = useDocumentData(q);
+  const q = doc(db, "posts", id);
+  const [post, isLoading] = useDocumentData(q);
 
-  return { post, isLoading }
+  return { post, isLoading };
 }
